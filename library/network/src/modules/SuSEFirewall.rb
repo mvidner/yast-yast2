f22e3db0d7aea3709b3a944145363abe485a44e4
Markos Chandras <mchandras@suse.de>
2016-03-02 09:45:29 +0000

    SuSEFirewall: Add FirewallD version of {Set,Get}Masquerade
    Add {Set,Get}Masquerade methods for FirewallD. However, this API
    is probably not very usable in its current form since FirewallD
    enables masquerade on per zone basis. So this API was extended to
    enable masquerade on the requested zones if such parameter is given.
    In order to maintain compatiblity with the SF2 callers, it also enables
    masquerade on all zones as well.

--- library/network/src/modules/SuSEFirewall.rb
+++ library/network/src/modules/SuSEFirewall.rb
@@ -1320,6 +1320,54 @@ module Yast
       nil
     end
 
+    # Function returns actual state of Masquerading support.
+    # In FirewallD, masquerade is enabled per-zone so this
+    # function has to iterate through all the zones and return
+    # a collective boolean value. This API is probably not very
+    # useful for FirewallD
+    #
+    # @param    zone to get masqurade status from
+    # @return	[Boolean] if supported
+    def GetMasquerade(zone = nil)
+      if zone.is_a?(::String)
+        if not IsKnownZone(zone)
+          Builtins.y2error("zone %1 is not valid", zone)
+          return nil
+        end
+        @SETTINGS[zone][:masquerade]
+      else
+        GetKnownFirewallZones().each do |z|
+          return false if not @SETTINGS[z][:masquerade]
+        end
+        true
+      end
+    end
+
+    # Function sets Masquerade support.
+    #
+    # @param	boolean to support or not to support it
+    # @param    Zone to enable masquerade on.
+    def SetMasquerade(enable, zone = nil)
+
+      if zone.is_a?(::String)
+        if not IsKnownZone(zone)
+          Builtins.y2error("zone %1 is not valid", zone)
+          return nil
+        end
+        SetModified()
+        @SETTINGS[zone][:masquerade] = enable
+        @SETTINGS[zone][:modified] << :masquerade
+      else
+        SetModified()
+        GetKnownFirewallZones().each do |z|
+          @SETTINGS[z][:masquerade] = enable
+          @SETTINGS[z][:modified] << :masquerade
+        end
+      end
+
+      nil
+    end
+
     publish variable: :firewall_service, type: "string", private: true
     publish variable: :FIREWALL_PACKAGE, type: "const string"
     publish variable: :SETTINGS, type: "map <string, any>", private: true
@@ -1369,6 +1417,8 @@ module Yast
     publish function: :SetInstallPackagesIfMissing, type: "void (boolean)"
     publish function: :SaveAndRestartService, type: "boolean ()"
     publish function: :GetProtectFromInternalZone, type: "boolean ()"
+    publish function: :GetMasquerade, type: "boolean (string)"
+    publish function: :SetMasquerade, type: "void (boolean, string)"
 
   end
 
@@ -3890,8 +3940,9 @@ module Yast
 
     # Function returns actual state of Masquerading support.
     #
+    # @param    Ignored
     # @return	[Boolean] if supported
-    def GetMasquerade
+    def GetMasquerade(zone = nil)
       Ops.get_string(@SETTINGS, "FW_MASQUERADE", "no") == "yes" &&
         Ops.get_string(@SETTINGS, "FW_ROUTE", "no") == "yes"
     end
@@ -3899,7 +3950,8 @@ module Yast
     # Function sets Masquerade support.
     #
     # @param	boolean to support or not to support it
-    def SetMasquerade(enable)
+    # @param    ignored
+    def SetMasquerade(enable, zone = nil)
       SetModified()
 
       Ops.set(@SETTINGS, "FW_MASQUERADE", enable ? "yes" : "no")

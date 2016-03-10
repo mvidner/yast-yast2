d151476b5bb61bad57b888ca5fbdd75fdd97f3b6
Markos Chandras <mchandras@suse.de>
2016-03-10 15:46:52 +0000

    SuSEFirewallServices: Add FirewallD versions for {Get,Set,Reset}Modify
    Move {Get,Reset}Modify methods to the main Firewall class and use
    a firewalld version for SetModify which prevents firewalld restarts
    on service changes. Changes to services take place immediately so there
    is no need to restart the firewall.

--- library/network/src/modules/SuSEFirewallServices.rb
+++ library/network/src/modules/SuSEFirewallServices.rb
@@ -142,6 +142,20 @@ module Yast
       DEFAULT_SERVICE.merge(service_details(service))
     end
 
+    # Sets that configuration was not modified
+    def ResetModified
+      @sfws_modified = false
+
+      nil
+    end
+
+    # Returns whether configuration was modified
+    #
+    # @return [Boolean] modified
+    def GetModified
+      @sfws_modified
+    end
+
   end
 
   class SuSEFirewall2ServicesClass < SuSEFirewallServicesClass
@@ -609,20 +623,6 @@ module Yast
       nil
     end
 
-    # Sets that configuration was not modified
-    def ResetModified
-      @sfws_modified = false
-
-      nil
-    end
-
-    # Returns whether configuration was modified
-    #
-    # @return [Boolean] modified
-    def GetModified
-      @sfws_modified
-    end
-
     # Function returns needed ports allowing broadcast
     #
     # @param [String] service
@@ -794,6 +794,9 @@ module Yast
 
       @known_metadata = { "Name" => "name", "Description" => "description" }
 
+      # firewall needs restarting. Always false for firewalld
+      @sfws_modified = false
+
     end
 
     # Reads definition of services that can be used in FirewallD
@@ -929,6 +932,14 @@ module Yast
       deep_copy(supported_services)
     end
 
+    # Sets that configuration was modified
+    def SetModified
+      # No need to restart firewalld on service changes.
+      @sfws_modified = false
+
+      nil
+    end
+
     publish function: :ReadServicesDefinedByRPMPackages, type: "boolean ()"
     publish function: :GetSupportedServices, type: "map <string, string> ()"
     publish function: :GetListOfServicesAddedByPackage, type: "list <string> ()"
@@ -939,6 +950,9 @@ module Yast
     publish function: :GetDescription, type: "string (string)"
     publish function: :IsKnownService, type: "boolean (string)"
     publish function: :GetNeededPortsAndProtocols, type: "map <string, list <string>> (string)"
+    publish function: :SetModified, type: "void ()"
+    publish function: :ResetModified, type: "void ()"
+    publish function: :GetModified, type: "boolean ()"
 
   end
 

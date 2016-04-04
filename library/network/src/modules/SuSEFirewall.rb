48218cbefe8952b735fb3a7de590c2bcfbb99ac3
Markos Chandras <mchandras@suse.de>
2016-04-04 10:52:48 +0100

    SuSEFirewall: Move AddXenSupport to main Firewall class
    SF2 and FirewallD can share the (deprecated) AddXenSupport method
    so move it to the main factory class.

--- library/network/src/modules/SuSEFirewall.rb
+++ library/network/src/modules/SuSEFirewall.rb
@@ -1116,6 +1116,19 @@ module Yast
       true
     end
 
+    # Function adds a special interface 'xenbr+' into the FW_FORWARD_ALWAYS_INOUT_DEV variable.
+    #
+    # @see #https://bugzilla.novell.com/show_bug.cgi?id=154133
+    # @see #https://bugzilla.novell.com/show_bug.cgi?id=233934
+    # @see #https://bugzilla.novell.com/show_bug.cgi?id=375482
+    def AddXenSupport
+      Builtins.y2milestone(
+        "The whole functionality is currently handled by SuSEfirewall2 itself"
+      )
+
+      nil
+    end
+
     # Create appropriate firewall instance based on factors such as which backends
     # are available and/or running/selected.
     # @return SuSEFirewall2 or SuSEFirewalld instance.
@@ -2393,6 +2406,7 @@ module Yast
     publish function: :HaveService, type: "boolean (string, string, string)"
     publish function: :AddService, type: "boolean (string, string, string)"
     publish function: :RemoveService, type: "boolean (string, string, string)"
+    publish function: :AddXenSupport, type: "void ()"
 
   end
 
@@ -4759,19 +4773,6 @@ module Yast
       SetModified()
 
       Ops.set(@SETTINGS, Ops.add("FW_IGNORE_FW_BROADCAST_", zone), bcast)
-
-      nil
-    end
-
-    # Function adds a special interface 'xenbr+' into the FW_FORWARD_ALWAYS_INOUT_DEV variable.
-    #
-    # @see #https://bugzilla.novell.com/show_bug.cgi?id=154133
-    # @see #https://bugzilla.novell.com/show_bug.cgi?id=233934
-    # @see #https://bugzilla.novell.com/show_bug.cgi?id=375482
-    def AddXenSupport
-      Builtins.y2milestone(
-        "The whole functionality is currently handled by SuSEfirewall2 itself"
-      )
 
       nil
     end
